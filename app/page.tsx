c1dc6d59033189d9e98576af7991b6de4222b00e
Harsha Reddy <harsha@droidnex.in>
2023-05-25 10:53:38 +0530

    tailwind css changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-     <h3> Hello Next </h3>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
